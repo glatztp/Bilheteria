fce6e1dd0ed55153d8f61ebafd08a0797dd14886
Glatz <143537581+glatztp@users.noreply.github.com>
2023-10-18 14:41:41 -0300

    Add files via upload

---
+++ ver.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<style type="text/css">
+body {
+    font-family: Arial, sans-serif;
+    background-color: #000;
+    margin: 0;
+    padding: 0;
+    color: #fff;
+}
+
+h2 {
+    text-align: center;
+    color: #ff4500;
+    margin-top: 20px;
+    margin-top: 90px;
+    margin-left: 20;
+}
+
+h1{
+	text-align: center;
+	color: #ff4500;  
+}
+
+img {
+    width: 175px;
+    height: auto;
+    border-radius: 5px;
+}
+
+.ranking-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    margin: 10px auto;
+    max-width: 1400px;
+    margin-top: 60px;
+
+}
+
+.ranking {
+    display: flex; /* Adicionado */
+    background-color: #333;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 10px;
+    text-align: justify;
+    width: 400px 900px;
+}
+
+p{
+	text-align: justify; 
+	margin-left: 50px;
+}
+h3 {
+    text-align: center;
+    color: #ff4500;
+    margin-top: 20px;
+        margin-right: 170px;
+}
+
+h4 {
+    text-align: justify;
+    color: #ff4500;
+    margin-top: 20px;
+    margin-left: 50px;
+}
+
+
+</style>
+<body>
+	<h1>Dados do Filme</h1>
+
+	<div class="ranking-container">
+
+	<div class="ranking">
+        <img src=https://th.bing.com/th/id/OIP.B4X2Gzt2QIDCB2ra2t0OtQHaK-?pid=ImgDet&rs=1 alt="Capa do filme Vingadores: Ultimato">
+        <div> <!-- Adicionado -->
+            <h3>Vingadores: Ultimato</h3>
+            <p>Arrecadação total - US$ 2,8 bilhões
+            <br>Custos produção - US$ 356 milhões</p>
+            <h4>Sinopse:</h4>
+            <p>Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark vagando perdido no espaço sem água nem comida, Steve Rogers e Natasha Romanov precisam liderar a resistência contra o titã louco.</p>
+        </div> <!-- Adicionado -->
+	</div>
+
+    <div class="ranking">
+        <img src=https://image.tmdb.org/t/p/original/8Y7WrRK1iQHEX7UIftBeBMjPjWD.jpg>
+        <div> <!-- Adicionado -->
+            <h3>Avatar</h3>
+            <p>Arrecadação total - US$ 2,789 bilhões²
+            <br>Custos produção - US$ 237 milhões³</p>
+            <h4>Sinopse:</h4>
+            <p>Avatar é um filme épico de ficção científica americano de 2009, dirigido, escrito, co-produzido e co-editado por James Cameron. O filme é estrelado por Sam Worthington, Zoe Saldana, Stephen Lang, Michelle Rodriguez e Sigourney Weaver. A história se passa em 2154, quando os humanos estão explorando Pandora, uma lua rica em biodiversidade no sistema estelar Alpha Centauri².</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/R.dad0169e533c061e0999285b3614a463?rik=bD7x1RbChOYasA&riu=http%3a%2f%2f2.bp.blogspot.com%2f-J_LatuSaoyk%2fT5wGZbNHc-I%2fAAAAAAAAFeY%2f1KVt3vEpX_4%2fs1600%2ftitanic%2bposter.jpg&ehk=t6iYS5WnHq8897gVebIMPTdKyT8PJd1HibXpZPwZVKI%3d&risl=&pid=ImgRaw&r=0>
+        <div> <!-- Adicionado -->
+            <h3>Titanic</h3>
+            <p>Arrecadação total - US$ 2.257 bilhões⁵
+            <br>Custos produção - US$ 200 milhões⁵</p>
+            <h4>Sinopse:</h4>
+            <p>Titanic é um filme de desastre épico americano de 1997 dirigido, escrito, co-produzido e co-editado por James Cameron. Incorporando aspectos históricos e ficcionalizados, é baseado em relatos do naufrágio do RMS Titanic em 1912. Kate Winslet e Leonardo DiCaprio estrelam como membros de diferentes classes sociais que se apaixonam durante a viagem inaugural do navio⁵.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/R.83ac5bbb93b82cf5232b1af8da26ce86?rik=snBXCfG1qLfAWg&pid=ImgRaw&r=0 alt="Capa do filme Vingadores: Ultimato">
+        <div> <!-- Adicionado -->
+            <h3>Star Wars</h3>
+            <p>Arrecadação total - US$ 2.068 bilhões (Star Wars: The Force Awakens)⁷
+            <br>Custos produção - US$ 245 milhões (Star Wars: The Force Awakens)⁷</p>
+            <h4>Sinopse:</h4>
+            <p>Star Wars é uma franquia de filmes de ficção científica criada pelo cineasta George Lucas que conta a saga da família Skywalker ao longo de três trilogias de filmes. A franquia começou com o lançamento do primeiro filme, Star Wars (mais tarde renomeado para Star Wars: Episode IV – A New Hope) em 1977⁷.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/R.572a22f560a169e909f94c6e32dc1b6a?rik=XhdxgOGIGQFSqw&pid=ImgRaw&r=0 alt="Capa do filme Vingadores: Ultimato">
+        <div> <!-- Adicionado -->
+            <h3>Vingadores: Guerra Infinita</h3>
+            <p>Arrecadação total - US$ 2,048 bilhões[^10^]
+            <br>Custos produção - US$ 321.2 milhões¹¹</p>
+            <h4>Sinopse:</h4>
+            <p>Vingadores: Guerra Infinita é um filme de super-herói americano baseado na equipe de super-heróis da Marvel Comics, os Vingadores. É a sequência de Os Vingadores (2012) e Vingadores: Era de Ultron (2015), e o décimo nono filme do Universo Cinematográfico Marvel (MCU). O filme é dirigido pelos irmãos Russo e apresenta um elenco repleto de estrelas incluindo Robert Downey Jr., Chris Hemsworth, Mark Ruffalo, Chris Evans e muitos outros. Na trama do filme, os Vingadores e os Guardiões da Galáxia tentam impedir Thanos de coletar as Joias do Infinito para acabar com metade da vida no universo[^10^]¹¹.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/OIP.Qpl57gJkPEDOqA6t4XMBcAHaJh?pid=ImgDet&rs=1>
+        <div> <!-- Adicionado -->
+            <h3>Jurassic World: O Mundo dos Dinossauros</h3>
+            <p>Arrecadação total - US$ 1,308,467,944
+            <br>Custos produção - US$ 170,000,000</p>
+            <h4>Sinopse:</h4>
+            <p>Jurassic World: O Mundo dos Dinossauros é um filme de aventura e ficção científica estadunidense de 2015, dirigido por Colin Trevorrow, sendo o quarto da franquia Jurassic Park e o primeiro de uma nova trilogia. O filme foi produzido pela Universal Pictures em parceria com a Amblin Entertainment, sendo distribuído pela Universal Pictures.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/OIP.u4ht0HqIiShMD5sM3ZXfoQHaKq?pid=ImgDet&rs=1>
+         <div> <!-- Adicionado -->
+            <h3>O Rei Leão</h3>
+            <p>Arrecadação total - US$ 1,656,943,394
+            <br>Custos produção - US$ 260,000,000</p>
+            <h4>Sinopse:</h4>
+            <p>O Rei Leão é um filme musical de drama estadunidense de 2019, dirigido e co-produzido por Jon Favreau e escrito por Jeff Nathanson, sendo o remake do longa-metragem animado O Rei Leão, de 1994, inspirado em partes da obra Hamlet, de William Shakespeare. É estrelado por Donald Glover, Seth Rogen, Chiwetel Ejiofor, Alfre Woodard, Billy Eichner, John Kani, John Oliver e Beyoncé Knowles-Carter.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/OIP.j8vpqMdwjJmRZV1YZE9WswHaLH?pid=ImgDet&rs=1 alt="Capa do filme Vingadores: Ultimato">
+        <div> <!-- Adicionado -->
+            <h3>Velozes & Furiosos 7</h3>
+            <p>Arrecadação total - US$ 1.5 bilhões
+            <br>Custos produção - US$ 190 milhões</p>
+            <h4>Sinopse:</h4>
+            <p>Velozes & Furiosos 7 é um filme de ação estadunidense de 2015 dirigido por James Wan e escrito por Chris Morgan. É o sétimo filme da franquia The Fast and the Furious e estrela Vin Diesel, Paul Walker (em sua última aparição nos cinemas), Dwayne Johnson e Michelle Rodriguez.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/OIP.OOXbKGarZesgvLCLv9wplwHaLH?pid=ImgDet&rs=1 alt="Capa do filme Vingadores: Ultimato">
+         <div> <!-- Adicionado -->
+            <h3>Frozen II</h3>
+            <p>Arrecadação total - US$ 1.450 bilhões
+            <br>Custos produção - US$ 150 milhões</p>
+            <h4>Sinopse:</h4>
+            <p>Frozen II é um filme de animação musical estadunidense de 2019 produzido pela Walt Disney Animation Studios e distribuído pela Walt Disney Studios Motion Pictures. É a sequência do filme Frozen (2013) e tem as vozes principais de Kristen Bell, Idina Menzel, Josh Gad e Jonathan Groff.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+	
+
+	</div>
+</body>
+</html>
+
+
+
+
+        
+        
+        
+       
+        
+       
